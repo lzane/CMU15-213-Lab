7385d49f8a4666a81b79428aa0f90d542d012ec9
lzane <cn.zanelee@gmail.com>
2023-04-04 13:23:15 +0800

    cachelab, optimize for case 2

--- cachelab-handout/trans.c
+++ cachelab-handout/trans.c
@@ -24,18 +24,82 @@ char transpose_submit_desc[] = "Transpose submission";
 void transpose_submit(int M, int N, int A[N][M], int B[M][N])
 {
     int B_col = 1<<(5-2);// each cache line can hold B_col int
-    int B_row = B_col;
+    int B_row;
 
+    // case 2
+    if(M==64&&N==64){
+        // because the cache can only contains 4 matrix lines, use 8x8 block size will cause 
+        // conflict misses when writing to B
+        B_row = 4;
+        B_col = 4;
+
+        for(int i=0; i<M; i+=B_row){
+            for(int j=0; j<N; j+=B_col){
+                // the following method has total miss 
+                // 4 read + 4 write = 8 per block
+
+                // A Matrix 
+                // [ A11  A12  A13  A14 ]
+                // [ A21  A22  A23  A24 ]
+                // [ A31  A32  A33  A34 ]
+                // [ A41  A42  A43  A44 ]
+                // B Matrix 
+                // [ B11  B12  B13  B14 ]
+                // [ B21  B22  B23  B24 ]
+                // [ B31  B32  B33  B34 ]
+                // [ B41  B42  B43  B44 ]
+
+                int ii = j, jj=i;
+                int t0,t1,t2,t3,t4,t5,t6,t7;                
+                
+                // [1] read set 0x9, 0x11
+                t0 = A[ii+0][jj+0];
+                t1 = A[ii+0][jj+1];
+                t2 = A[ii+1][jj+0];
+                t3 = A[ii+1][jj+1];
+                // cache to later use
+                t4 = A[ii+0][jj+2];
+                t5 = A[ii+0][jj+3];
+                t6 = A[ii+1][jj+2];
+                t7 = A[ii+1][jj+3];
+
+                // [2] write set 0x9, 0x11 
+                B[i+0][j]=t0;
+                B[i+0][j+1]=t2;
+                B[i+1][j]=t1;
+                B[i+1][j+1]=t3;
+
+                // [3] write set 0x9, 0x11, read set 0x19, 0x01
+                B[i+0][j+2] = A[ii+2][jj+0];
+                B[i+0][j+3] = A[ii+3][jj+0];
+                B[i+1][j+2] = A[ii+2][jj+1];
+                B[i+1][j+3] = A[ii+3][jj+1];
+                // cache for later use
+                t0 = A[ii+2][jj+2];
+                t1 = A[ii+2][jj+3];
+                t2 = A[ii+3][jj+2];
+                t3 = A[ii+3][jj+3];
+
+                // [4] write set 0x19, 0x01
+                B[i+2][j+0] = t4;
+                B[i+2][j+1] = t6;
+                B[i+2][j+2] = t0;
+                B[i+2][j+3] = t2;
+                B[i+3][j+0] = t5;
+                B[i+3][j+1] = t7;
+                B[i+3][j+2] = t1;
+                B[i+3][j+3] = t3;
+            }
+        }
+        return; 
+    }
+
+    // case 1
     if(M==32&&N==32){
         B_row = 8;
     }
 
-    if(M==64&&N==64){
-        // because the cache can only contains 4 matrix lines, use 8x8 block size will cause 
-        // conflict misses when writing to B
-        B_row = 4; 
-    }
-
+    // case 3
     if(M==61&&N==67){
         B_row = 8;
     }
